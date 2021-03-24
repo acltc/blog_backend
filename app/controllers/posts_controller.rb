462e2b79beb61d96dc0be7c8cb74b77c806b99e8
Peter Jang <peter.x.jang@gmail.com>
2021-03-24 13:29:52 -0500

    Add posts CRUD actions

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
+
+  def index
+    posts = Post.all
+    render json: posts.as_json
+  end
+
+  def create
+    post = Post.new(
+      title: params[:title],
+      body: params[:body],
+      image: params[:image],
+      user_id: current_user.id,
+    )
+    if post.save
+      render json: post.as_json
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    post = Post.find_by(id: params[:id])
+    render json: post.as_json
+  end
+
+  def update
+    post = Post.find_by(id: params[:id])
+    post.title = params[:title] || post.title
+    post.body = params[:body] || post.body
+    if post.save
+      render json: post.as_json
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = Post.find_by(id: params[:id])
+    post.destroy
+    render json: { message: "Post successfully destroyed!" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
+  get "/posts" => "posts#index"
+  post "/posts" => "posts#create"
+  get "/posts/:id" => "posts#show"
+  patch "/posts/:id" => "posts#update"
+  delete "/posts/:id" => "posts#destroy"
 end
